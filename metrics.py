7ce9db286723f8d9e43c22593e42148f16a711e8
kersoftwal <t.franza.fg@gmail.com>
2021-06-22 18:52:44 +0200

    updated models module, updated training module to call model building

--- metrics.py
+++ metrics.py
@@ -7,7 +7,7 @@ from sklearn.metrics import recall_score as recall
 from sklearn.metrics import precision_score as precision
 from sklearn.metrics import f1_score
 
-def get_metrics(labels: list(str), y_true: np.array, y_pred: np.array) -> dict:
+def get_metrics(labels: list, y_true: np.array, y_pred: np.array) -> dict:
     '''
     Extracts the metrics according to the true labels and the predictions obtained from the classifier.
 
@@ -16,27 +16,21 @@ def get_metrics(labels: list(str), y_true: np.array, y_pred: np.array) -> dict:
     :param y_pred: matrix of matching labels (that the classifier has predicted) 
     
     :return: dictionary of metrics. Each entry value is a list of floats with length equal to the amount of labels. 
-    '''
-    metrics = {
-        'name': []
-        'acc': [],
-        'rec': [],
-        'prec': [],
-        'f1': []
-        'cm': [],
-    }
+    '''    
+    name, acc, cm, rec, prec, f1 = [], [], [], [], [], []
     for i, label in enumerate(labels): 
-        acc   = accuracy(y_true[:,i], y_pred[:,i]) 
-        cmlog = confusion_matrix(y_true[:,i], y_pred[:,i])
-        rec   = recall(y_true[:,i], y_pred[:,i])
-        prec  = precision(y_true[:,i], y_pred[:,i])
-        f1    = f1_score(y_true[:,i], y_pred[:,i])
+        name = name + [label]
+        acc = acc + [accuracy(y_true[:,i], y_pred[:,i])]
+        cm = cm + [confusion_matrix(y_true[:,i], y_pred[:,i])]
+        rec = rec + [recall(y_true[:,i], y_pred[:,i])]
+        prec = prec + [precision(y_true[:,i], y_pred[:,i])]
+        f1 = f1 + f1_score(y_true[:,i], y_pred[:,i])
 
-        metrics['name'] = metrics['name'].append(label)
-        metrics['acc'] = metrics['acc'].append(acc)
-        metrics['cmlog'] = metrics['cmlog'].append(cmlog)
-        metrics['rec'] = metrics['rec'].append(rec)
-        metrics['prec'] = metrics['prec'].append(prec)
-        metrics['f1'] = metrics['f1'].append(f1)
-
-    return metrics
+    return dict({
+        'name': name,
+        'acc': acc,
+        'rec': rec,
+        'prec': prec,
+        'f1': f1,
+        'cm': cm
+    })
--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-
 import json
 import numpy as np
 
@@ -8,99 +7,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.svm import LinearSVC, SVC
 
-from ./metrics import get_metrics
+from metrics import get_metrics
 
 FEATURES = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 
-def logreg(self, random_state: int=0) -> SklearnSupervisedModel:
-    '''
-    Builds the Logistic Regression classifier with ad-hoc chosen parameters for the available data. 
-    In order to deal with the multilabel task, it uses OneVsRestClassifier on top of the chosen model.
-        
-    :param random_state: random state to make experiments replicable.
-
-    :return: sklearn supervised model encapsulated into the SklearnSupervisedModel class.
-    '''
-        
-    model = OneVsRestClassifier(
-        LogisticRegression(
-            penalty = 'l2',
-            C = 9,
-            solver = 'liblinear',
-            class_weight = 'balanced',
-            random_state = random_state
-        )
-    )
-    return SklearnSupervisedModel(model=model, name='logreg')
-
-def svc_linear(self, random_state: int=0) -> SklearnSupervisedModel:
-    '''
-    Builds the Linear Support Vector classifier with ad-hoc chosen parameters for the available data. 
-    In order to deal with the multilabel task, it uses OneVsRestClassifier on top of the chosen model.
-            
-    :param random_state: random state to make experiments replicable.
-    
-    :return: sklearn supervised model encapsulated into the SklearnSupervisedModel class.
-    '''
-    model = OneVsRestClassifier(
-        LinearSVC(
-            dual = False, 
-            class_weight = 'balanced',
-            random_state = random_state
-        )
-    )
-    return SklearnSupervisedModel(model=model, name='svc_linear')
-
-def svc_poly(self, random_state: int=0) -> SklearnSupervisedModel:
-    '''
-    Builds the Support Vector classifier with polynomial features and ad-hoc chosen parameters for the available data. 
-    In order to deal with the multilabel task, it uses OneVsRestClassifier on top of the chosen model.
-            
-    :param random_state: random state to make experiments replicable.
-    
-    :return: sklearn supervised model encapsulated into the SklearnSupervisedModel class.
-    '''
-    model = OneVsRestClassifier(
-        SVC(
-            kernel='poly', 
-            random_state=random_state
-        )
-    )
-    return SklearnSupervisedModel(model=model, name='svc_poly')
-
-def randomforest(self, random_state: int=0) -> SklearnSupervisedModel:
-    '''
-    Builds the RandomForest classifier with ad-hoc chosen parameters for the available data. 
-    In order to deal with the multilabel task, it uses OneVsRestClassifier on top of the chosen model.
-            
-    :param random_state: random state to make experiments replicable.
-
-    :return: sklearn supervised model encapsulated into the SklearnSupervisedModel class.
-    '''
-    model = OneVsRestClassifier(
-        RandomForestClassifier(
-            class_weight='balanced',
-            random_state=random_state
-        )
-    )
-    return SklearnSupervisedModel(model=model, name='randomforest')
-
-def xgboost(self, random_state: int=0) -> SklearnSupervisedModel:
-    '''
-    Builds the Gradient Boosting classifier with ad-hoc chosen parameters for the available data. 
-    In order to deal with the multilabel task, it uses OneVsRestClassifier on top of the chosen model.
-            
-    :param random_state: random state to make experiments replicable.
-
-    :return: sklearn supervised model encapsulated into the SklearnSupervisedModel class.
-    '''
-    model = OneVsRestClassifier(
-        GradientBoostingClassifier(
-            max_depth=9,
-            random_state=random_state
-        )
-    )
-    return SklearnSupervisedModel(model=model, name='xgboost')
 
 class SklearnSupervisedModel:
     '''
@@ -114,11 +24,11 @@ class SklearnSupervisedModel:
         :param model: Sklearn model to fit.
         :param name: Name of the model. Useful for saving and loading purposes.
         '''
-        super().__init__(self)
+        super(SklearnSupervisedModel, self).__init__()
         self.model = model
         self.name = name
 
-        def fit(self, train_X: np.array, train_Y: np.array):
+    def fit(self, train_X: np.array, train_Y: np.array):
         '''
         Fits the classifier to the training set. Updates the :attr: `model`.
     
@@ -138,7 +48,7 @@ class SklearnSupervisedModel:
         self.testset = (test_X, None)
         self.pred = self.model.predict(test_X)
 
-    def eval(self, test_Y: np.array, save_eval: bool, verbose: bool=True) -> dict(str):
+    def eval(self, test_Y: np.array, save_eval: bool, verbose: bool=True) -> dict:
         '''
         Evaluates the predictions obtained from the classifier with the real labels.
         
@@ -150,17 +60,21 @@ class SklearnSupervisedModel:
         '''
         self.testset = (self.testset[0], test_Y)
         self.metrics = get_metrics(FEATURES, test_Y, self.pred)
-
+        
         if verbose:
-            print(f' \
-                Feature = {self.metrics['name']}, \
-                Accuracy = {np.round(self.metrics['acc'],5)}, \
-                Precision = {np.round(self.metrics['prec'],3)}, \
-                Recall = {np.round(self.metrics['rec'],3)}, \
-                F1-score = {np.round(self.metrics['f1'],3)}'
-            )
-            print('Confusion Matrix is:')
-            print(self.metrics['cm'], '\n')
+            for i, label in enumerate(self.metrics['name']):
+                acc = self.metrics['acc'][i]
+                prec = self.metrics['prec'][i]
+                rec = self.metrics['rec'][i]
+                f1 = self.metrics['f1'][i]
+                print(f' \
+                    Feature = {label}, \
+                    Accuracy = {np.round(acc,5)}, \
+                    Precision = {np.round(prec,5)}, \
+                    Recall = {np.round(rec,5)}, \
+                    F1-score = {np.round(f1,5)}'
+                )
+                print('Confusion Matrix is:\n', self.metrics['cm'][i], '\n')
 
         if save_eval:
             with open(f'results/{self.name}', 'w') as handle:
@@ -176,4 +90,134 @@ class SklearnSupervisedModel:
         with open(f'models/{self.name}.model', 'w') as handle:
             pickle.dump(self.model, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+
+class ModelLoader:
+    
+    def __init__(self, random_state):
+        '''
+        Builder. Assigns the random state to the corresponding attribute.
+        
+        :param random_state: random state to make experiments replicable.
+        '''
+        super(ModelLoader, self).__init__()
+        
+        self.random_state = random_state
+
+    def load_model(self, model_name) -> SklearnSupervisedModel:
+        '''
+        Builds and returns the model corresponding to the model name passed as input.
+            
+        :param model_name: name of the model to build.
+
+        :return: sklearn supervised model initialized.
+        '''
+        print(f' > LOADING MODEL {model_name} ...')
+        return eval(f'self.{model_name}()')
+
+#        if model_name == 'logreg':
+#            return self.logreg()
+#        elif model_name == 'svc_linear':
+#            return self.svc_linear()
+#        elif model_name == 'svc_poly':
+#            return self.svc_poly()
+#        elif model_name == 'randomforest':
+#            return self.randomforest()
+#        elif model_name == 'xgboost':
+#            return self.xgboost()
+#        else:
+#            print(f'   - {model_name} not found!')
+
+
+    ###################################################
+    ##### MODELS
+
+    def logreg(self) -> SklearnSupervisedModel:
+        '''
+        Builds the Logistic Regression classifier with ad-hoc chosen parameters for the available data. 
+        In order to deal with the multilabel task, it uses OneVsRestClassifier on top of the chosen model.
+            
+        :param model_name: name of the model to build.
+        :attr random_state: random state to make experiments replicable.
+
+        :return: sklearn supervised model encapsulated into the SklearnSupervisedModel class.
+        ''' 
+        model = OneVsRestClassifier(
+            LogisticRegression(
+                penalty = 'l2',
+                C = 9,
+                solver = 'liblinear',
+                class_weight = 'balanced',
+                random_state = self.random_state
+            )
+        )
+        return SklearnSupervisedModel(model=model, name='logreg')
+
+    def svc_linear(self) -> SklearnSupervisedModel:
+        '''
+        Builds the Linear Support Vector classifier with ad-hoc chosen parameters for the available data. 
+        In order to deal with the multilabel task, it uses OneVsRestClassifier on top of the chosen model.
+                
+        :param random_state: random state to make experiments replicable.
+        
+        :return: sklearn supervised model encapsulated into the SklearnSupervisedModel class.
+        '''
+        model = OneVsRestClassifier(
+            LinearSVC(
+                dual = False, 
+                class_weight = 'balanced',
+                random_state = self.random_state
+            )
+        )
+        return SklearnSupervisedModel(model=model, name='svc_linear')
+
+    def svc_poly(self) -> SklearnSupervisedModel:
+        '''
+        Builds the Support Vector classifier with polynomial features and ad-hoc chosen parameters for the available data. 
+        In order to deal with the multilabel task, it uses OneVsRestClassifier on top of the chosen model.
+                
+        :param random_state: random state to make experiments replicable.
+        
+        :return: sklearn supervised model encapsulated into the SklearnSupervisedModel class.
+        '''
+        model = OneVsRestClassifier(
+            SVC(
+                kernel='poly', 
+                random_state=self.random_state
+            )
+        )
+        return SklearnSupervisedModel(model=model, name='svc_poly')
+
+    def randomforest(self) -> SklearnSupervisedModel:
+        '''
+        Builds the RandomForest classifier with ad-hoc chosen parameters for the available data. 
+        In order to deal with the multilabel task, it uses OneVsRestClassifier on top of the chosen model.
+                
+        :param random_state: random state to make experiments replicable.
+
+        :return: sklearn supervised model encapsulated into the SklearnSupervisedModel class.
+        '''
+        model = OneVsRestClassifier(
+            RandomForestClassifier(
+                class_weight='balanced',
+                random_state=self.random_state
+            )
+        )
+        return SklearnSupervisedModel(model=model, name='randomforest')
+
+    def xgboost(self) -> SklearnSupervisedModel:
+        '''
+        Builds the Gradient Boosting classifier with ad-hoc chosen parameters for the available data. 
+        In order to deal with the multilabel task, it uses OneVsRestClassifier on top of the chosen model.
+                
+        :param random_state: random state to make experiments replicable.
+
+        :return: sklearn supervised model encapsulated into the SklearnSupervisedModel class.
+        '''
+        model = OneVsRestClassifier(
+            GradientBoostingClassifier(
+                max_depth=9,
+                random_state=self.random_state
+            )
+        )
+        return SklearnSupervisedModel(model=model, name='xgboost')
 
